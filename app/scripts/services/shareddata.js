193fc996a5b6d1d8fe83ad788972c9ffed1a93af
Ankit Verma <ankit.b.verma@accenture.com>
2015-05-19 17:55:15 +0530

    vehicle navigation changes

--- app/scripts/services/shareddata.js
+++ app/scripts/services/shareddata.js
@@ -12,49 +12,49 @@ angular.module('vidhataHondaApp')
   .service('SharedData', function () {
     var carDetails =
   {
-  "defaultPages":
+  'defaultPages':
   [
-    {"title":"Exteriors"},
-    {"title":"Features"},
-    {"title":"Specifications"},
-    {"title":"Engine & Performance"},
-    {"title":"Safety"},
-    {"title":"Accessories"},
-    {"title":"Price Range"}
+    {'title':'Exteriors'},
+    {'title':'Features'},
+    {'title':'Specifications'},
+    {'title':'Engine & Performance'},
+    {'title':'Safety'},
+    {'title':'Accessories'},
+    {'title':'Price Range'}
   ],
-  "cars":[
+  'cars':[
       {
-        "carName": "Honda-Amaze",
-        "image":"/images/homepage/slider/Honda-Amaze.jpg",
-        "landingPage":"#/Honda-Amaze",
+        'carName': 'Honda-Amaze',
+        'image':'/images/homepage/slider/Honda-Amaze.jpg',
+        'landingPage':'#/Honda-Amaze',
       },
       {
-        "carName": "Honda-Brio",
-        "image":"/images/homepage/slider/Honda-Brio.jpg",
-        "landingPage":"#/Honda-Brio",
+        'carName': 'Honda-Brio',
+        'image':'/images/homepage/slider/Honda-Brio.jpg',
+        'landingPage':'#/Honda-Brio',
       },
       {
-        "carName": "Honda-City",
-        "image":"/images/homepage/slider/Honda-City.jpg",
-        "landingPage":"#/Honda-City",
+        'carName': 'Honda-City',
+        'image':'/images/homepage/slider/Honda-City.jpg',
+        'landingPage':'#/Honda-City',
       },
       {
-        "carName": "Honda-Crv",
-        "image":"/images/homepage/slider/Honda-Crv.jpg",
-        "landingPage":"#/Honda-Crv",
+        'carName': 'Honda-Crv',
+        'image':'/images/homepage/slider/Honda-Crv.jpg',
+        'landingPage':'#/Honda-Crv',
       },
       {
-        "carName": "Honda-Jazz",
-        "image":"/images/homepage/slider/Honda-Jazz.jpg",
-        "landingPage":"#/Honda-Jazz",
+        'carName': 'Honda-Jazz',
+        'image':'/images/homepage/slider/Honda-Jazz.jpg',
+        'landingPage':'#/Honda-Jazz',
       },
       {
-        "carName": "Honda-Mobilio",
-        "image":"/images/homepage/slider/Honda-Mobilio.jpg",
-        "landingPage":"#/Honda-Mobilio",
+        'carName': 'Honda-Mobilio',
+        'image':'/images/homepage/slider/Honda-Mobilio.jpg',
+        'landingPage':'#/Honda-Mobilio',
       }
     ]
-}
+};
     return {getCarDetails: function () {
                 return carDetails;
             }
